12535066f4107a694aa03a7d4f907f59c7eaa081
Shinichi Okada <okada.shin@gmail.com>
2023-07-12 20:29:25 +0200

    fix: typo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -62,7 +62,7 @@
   <div class="sm:flex sm:items-center sm:justify-between">
     <FooterBrand
     href="https://svelte-coreui-icons.vercel.app/"
-    name="Flowbite Svelte Icons"
+    name="Svelte Coreui Icons"
   />
   <FooterLinkGroup ulClass="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
     <FooterLink href="/brand">Brand</FooterLink>
